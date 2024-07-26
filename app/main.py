3b7db083a3d153b87fd42b4f9dbbe8b9ff5384ae
Alina Murzakova <alinablb@yandex.ru>
2024-07-26 15:44:24 +0300

    Added function read_array and added display of wells in function save_img

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 from map import mapping
+from input_output import input_data
 from loguru import logger
 
 
 if __name__ == '__main__':
     logger.add('logs.log')
+    logger.info("Загрузка скважинных данных")
+    data_history, data_wells = input_data(data_well_directory=r"C:\Users\Alina\Desktop\Python\!Работа IT ННГ\Infill_drilling\Infill_drilling\input_files\Крайнее_Ю1\Крайнее_Ю1.xlsx")
     logger.info("Обработка карт")
-    mapping(maps_directory="D:/Work/Programs_Python/Infill_drilling/input/Крайнее_Ю1/Ascii grid grd",
-            save_directory="D:/Work/Programs_Python/Infill_drilling/output/")
+    mapping(maps_directory=r"C:/Users/Alina/Desktop/Python/!Работа IT ННГ/Infill_drilling/Infill_drilling/input_files/Крайнее_Ю1/Ascii grid grd",
+            save_directory=r"C:/Users/Alina/Desktop/Python/!Работа IT ННГ/Infill_drilling/Infill_drilling/output/",
+            data_wells=data_wells)
